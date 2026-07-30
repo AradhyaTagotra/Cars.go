ecd149b5199f60d18ced6ff48411451142094750
AradhyaTagotra <aradhya.ycet@gmail.com>
2026-07-31 01:58:28 +0530

    Add car listing frontend with  car data

---
+++ src/data/dummyCars.js
@@ -0,0 +1,8 @@
+export const dummyCars = [
+  { id: 1, make: "Honda", year: 2019, price: 650000 , doors: "4", mileage: "20", seats:"5",battery:"Hybrid"},
+  { id: 2, make: "Audi", year: 2017, price: 350000 , doors: "2", mileage: "10", seats:"2",battery:"Electric"},
+  { id: 3, make: "Hyundai", year: 2016, price: 550000 , doors: "4", mileage: "15", seats:"4",battery:"Electric"},
+  { id: 4, make: "Toyota", year: 2018, price: 450000 , doors: "4", mileage: "16", seats:"7",battery:"hybrid"},
+  { id: 5, make: "BMW", year: 2020, price: 750000 , doors: "4", mileage: "12", seats:"4",battery:"hybrid"},
+  { id: 6, make: "Mercedes", year: 2021, price: 850000 , doors: "4", mileage: "10", seats:"4",battery:"Electric"},
+];
