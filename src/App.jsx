20f318d8a29f05ab26f1bf0098cf4785225a610e
AradhyaTagotra <aradhya.ycet@gmail.com>
2026-08-04 21:43:27 +0530

    Render full car grid instead of single card

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { dummyCars } from "./data/dummyCars";
 import CarCard from "./components/CarCard";
 import "./components/CarCard.css";
+import "./App.css";
 
 function App() {
   const car = dummyCars[0];
   return (
-  //   <div>
-  //     <h1>Car Listings</h1>
-  //     {dummyCars.map(car => (
-  // <p key={car.id}>{car.make} {car.mileage} {car.battery} ({car.year}) - £{car.price}</p>
-  //   ))}
-  //   </div>
-  
-    <CarCard car={car}/>
+    //   <div>
+    //     <h1>Car Listings</h1>
+    //     {dummyCars.map(car => (
+    // <p key={car.id}>{car.make} {car.mileage} {car.battery} ({car.year}) - £{car.price}</p>
+    //   ))}
+    //   </div>
+    <div className="car-grid">
+      {dummyCars.map(car => (
+        <CarCard key={car.id} car={car}/>
+      ))}
+       
+    </div>
   );
 }
 
