96c034817838aec424b8f87aee918b98f99ab605
AradhyaTagotra <aradhya.ycet@gmail.com>
2026-08-04 21:47:45 +0530

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import "./App.css";
 function App() {
   const car = dummyCars[0];
   return (
-    //   <div>
-    //     <h1>Car Listings</h1>
-    //     {dummyCars.map(car => (
-    // <p key={car.id}>{car.make} {car.mileage} {car.battery} ({car.year}) - £{car.price}</p>
-    //   ))}
-    //   </div>
     <div className="car-grid">
       {dummyCars.map(car => (
         <CarCard key={car.id} car={car}/>
