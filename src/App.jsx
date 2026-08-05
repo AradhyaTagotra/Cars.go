87c68394765f3cee78be837068466d7ae3510bf5
AradhyaTagotra <aradhya.ycet@gmail.com>
2026-08-05 21:34:03 +0530

    included search bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,37 @@ import { dummyCars } from "./data/dummyCars";
 import CarCard from "./components/CarCard";
 import "./components/CarCard.css";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [filteredCars, setFilteredCars] = useState(dummyCars);
+
+  const handleSearch = () => {
+    const results = dummyCars.filter(car =>
+      car.make.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredCars(results);
+  };
+
   return (
-    <div className="car-grid">
-      {dummyCars.map(car => (
-        <CarCard key={car.id} car={car}/>
-      ))}
-       
+    <div>
+      <input
+        type="text"
+        placeholder="Search by make"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      
+      <div className="car-grid">
+        {dummyCars.map(car => (
+          <CarCard key={car.id} car={car} />
+        ))}
+
+      </div>
     </div>
+
   );
 }
 
