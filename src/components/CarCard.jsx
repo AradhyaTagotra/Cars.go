9f816d6013bca3f1c0e333397cac2e5e394ae7a8
AradhyaTagotra <aradhya.ycet@gmail.com>
2026-08-08 00:22:00 +0530

    Implemented with real vehcile schema

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,14 +1,16 @@
 import "./CarCard.css";
-function CarCard({car}){
-    return(
+function CarCard({ car }) {
+    return (
         <div className="car-card">
-         <img src={car.image} alt={`${car.make}`} className="car-image" />
-         <div className="car-info">
-            <h3 className="car-title">{car.make} ({car.year})</h3>
-            <p className="car-price">£{car.price.toLocaleString()}</p>
-            <p className="car-details">{car.mileage.toLocaleString()} km · {car.battery} · {car.doors} doors · {car.seats} seats</p>
-         </div>
+            <img src={car.images[0].path} alt={`${car.make} ${car.model}`} className="car-image" />
+            <div className="car-info">
+                <h3 className="car-title">{car.make} {car.model} ({car.firstRegistrationYear})</h3>
+                <p className="car-price">£{car.price.toLocaleString()}</p>
+                <p className="car-details">
+                    {car.mileage.toLocaleString()} km · {car.fuelType} · {car.gearboxType} · {car.noOfDoors} doors · {car.noOfSeats} seats
+                </p>
+            </div>
         </div>
-    );  
+    );
 }
 export default CarCard;
